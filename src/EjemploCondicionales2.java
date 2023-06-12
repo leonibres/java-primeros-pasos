9671f373ac9d76422a2da8881269ca3651302c87
leonibres <lejimenez8@poligran.edu.co>
2023-06-12 12:19:35 -0500

    first commit

---
+++ src/EjemploCondicionales2.java
@@ -0,0 +1,14 @@
+public class EjemploCondicionales2 {
+
+	public static void main(String[] args) {
+		int edad = 21;
+		int cantidadPersonas = 1;
+
+		if ( edad >= 18 && cantidadPersonas > 1 ) {
+			System.out.println("Usted puede entrar");
+		} else {
+			System.out.println("Usted no puede entrar");
+		}
+	}
+}
+
